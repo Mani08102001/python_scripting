eef200f8d515ee364987fc1df992cffc9aa2848d
Manikandan P <85947276+Mani08102001@users.noreply.github.com>
2024-04-18 21:42:48 +0530

    Update test.py

--- github_integration/test.py
+++ github_integration/test.py
@@ -1,9 +1,24 @@
 import requests
 
+url = f'https://api.github.com/repos/kubernetes/kubernetes/pulls'
 
-response=requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
+response = requests.get(url) 
 
-complete_details = response.json()
+if response.status_code == 200:
+   
+    pull_requests = response.json()
 
-for i in range(len(complete_details)):
-    print(complete_details[i]["user"]["login"])
+    pr_creators = {}
+
+    for pull in pull_requests:
+        creator = pull['user']['login']
+        if creator in pr_creators:
+            pr_creators[creator] += 1
+        else:
+            pr_creators[creator] = 1
+
+    print("PR Creators and Counts:")
+    for creator, count in pr_creators.items():
+        print(f"{creator}: {count} PR(s)")
+else:
+    print(f"Failed to fetch data. Status code: {response.status_code}")
